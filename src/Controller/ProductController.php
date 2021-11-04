688e95a01bda969b24057416cec7fb0b6eb4f97b
Cyliana <a.taddei@codeur.online>
2021-11-04 09:50:12 +0100

    continuation of the exercises

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,13 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
-/**
- * @Route("/product", name="product_")
- */
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/product", name="product_index")
      */
     public function index(ProductRepository $productRepository): Response
     {
@@ -30,7 +27,7 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="show", priority = -1)
+     * @Route("/product/{id}", name="product_show", priority=-1)
      */
     public function show($id, ProductRepository $productRepository) : response
     {
@@ -49,19 +46,21 @@ class ProductController extends AbstractController
 
 
     /**
-     * @Route("/add", name="add", methods={"GET", "POST"})
+     * @Route("/product/add", name="product_add", methods={"GET","POST"})
      */
     public function add(Request $request)
     {
-        $product = new Product();
+        $product = new Product;
         $formProduct = $this-> createForm(ProductType::class, $product);
         $formProduct->handleRequest($request);
 
-        if($formProduct->isSubmitted()&& $formProduct->isValid())
+        if($formProduct->isSubmitted() && $formProduct->isValid())
         {
-            $product->setCreatedAt( new \DateTime());
-            $product->setUpdatedAt($product->getCreatedAt());
-            
+           dd($formProduct);
+
+            $product->setCreatedAt( new \DateTime())
+                ->setUpdatedAt($product->getCreatedAt());
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($product);
             $entityManager->flush();
@@ -72,6 +71,42 @@ class ProductController extends AbstractController
         }
 
         return $this->renderForm('product/add.html.twig', [
+            'formProduct' => $formProduct
+        ]);
+    }
+
+    /**
+     * @Route("/product/edit/{id}", name="product_edit", methods={"GET", "POST"})
+     */
+    public function edit($id, ProductRepository $productRepository, Request $request)
+    {
+        $product = $productRepository->find($id);
+
+        if(!$product) 
+        {
+            throw $this->createNotFoundException("Le produit à modifier n'existe pas.");
+        }
+
+        $formProduct = $this-> createForm(ProductType::class, $product);
+
+        $formProduct->handleRequest($request);
+
+        if($formProduct->isSubmitted()&& $formProduct->isValid())
+        {
+            $product->setUpdatedAt(new \DateTime());
+            
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash('success','Le produit à été modifié');
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->renderForm('product/edit.html.twig', [
+            "action" => "Modifier",
             'formProduct' => $formProduct
         ]);
     
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,57 +2,16 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Category;
-use Faker\Factory;
-use App\Entity\Product;
-use Bezhanov\Faker\Provider\Commerce;
-use Bluemmb\Faker\PicsumPhotosProvider;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
+        // $product = new Product();
+        // $manager->persist($product);
 
-        $faker = Factory::create();
-        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
-        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
-
-        $colorCategory= ['success','danger','warning','info','dark'];
-
-        for ($c=0; $c < 5 ; $c++) 
-        { 
-            $category = new Category();
-
-            $category->setName($faker->department())
-                     ->setColor($colorCategory[$c]);
-
-            $manager->persist($category);
-            
-            for ($p=1; $p < 10 ; $p++) 
-            { 
-                $product = new Product();
-
-                $date = $faker->dateTimeThisYear();
-
-                $product->setName($faker->productName())
-                        ->setShortDescription($faker->sentence(20))
-                        ->setPrice($faker->randomFloat(2,10,300))
-                        ->setLongDescription($faker->paragraph(50))
-                        ->setQuantity($faker->numberBetween(0,50))
-                        ->setVisible($faker->boolean())
-                        ->setCreatedAt($date)
-                        ->setUpdatedAt($date)
-                        ->setMainPicture($faker->imageUrl(300, 300, true))
-                        ->setCategory($category);
-                        
-
-                $manager->persist($product);            
-            }
-            $manager->flush();
-        } 
+        $manager->flush();
     }
 }
-
